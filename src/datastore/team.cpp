ab918927ee8bc296de386ce18a5fbf9a944eaaf5
Christopher Rzechula <christopherrzechula@gmail.com>
2019-04-02 19:41:28 -0700

    Check for invalid name in team ctor

--- src/datastore/team.cpp
+++ src/datastore/team.cpp
@@ -12,6 +12,7 @@ Team::Team()
 /**
  * Constructs a team given a name and league. The ID of this team
  * will be set to @a nextId, then @a nextId is incremented after.
+ * If @a name is an empty string, the team's name is set to "invalid".
  *
  * The stadium ID is set to -1 to indicate that the team is not
  * in a stadium yet.
@@ -20,9 +21,11 @@ Team::Team()
  * @param leag Team's league
  */
 Team::Team(const std::string& name, League leag)
-    : league(leag), m_id(nextId), m_name(name)
+    : league(leag), m_id(nextId)
 {
     nextId++;
+
+    setName(name);
 }
 
 int Team::getId() const
