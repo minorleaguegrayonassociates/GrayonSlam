e6227ecbc53775d6b92fa8898c01cb62d1ae90c9
Roger Chavez <roger.chavez.03@gmail.com>
2019-04-12 15:25:36 -0700

    Removed unnecesarry headers from the parser header file

--- src/utils/parser.hpp
+++ src/utils/parser.hpp
@@ -2,9 +2,6 @@
 
 #include <vector>
 #include <string>
-#include "../datastore/souvenir.hpp"
-#include "../datastore/stadium.hpp"
-#include "../datastore/team.hpp"
 
 std::vector<std::vector<std::string>> loadData(const std::string&);
 void saveData(const std::string&, const std::vector<std::vector<std::string>>&);
