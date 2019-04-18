599c168f9a8f89888ff6d508eae516ce5cabac99
yosmo78 <yosmo78@gmail.com>
2019-04-17 22:29:01 -0700

    ok really readded files now

--- src/widgets/stadiumlist.cpp
+++ src/widgets/stadiumlist.cpp
@@ -4,3 +4,9 @@ StadiumList::StadiumList()
 {
 
 }
+
+
+
+
+
+
--- src/widgets/stadiumlist.hpp
+++ src/widgets/stadiumlist.hpp
@@ -1,11 +1,26 @@
 #ifndef STADIUMLIST_HPP
 #define STADIUMLIST_HPP
 
+#include <QMainWindow>
+#include "src/datastore/stadium.hpp"
+#include "src/datastore/team.hpp"
 
 class StadiumList
 {
+    Q_OBJECT
 public:
-    StadiumList();
+    explicit StadiumList();
+    StadiumList(const std::vector<Stadium>&);
+    std::vector<Stadium>& getStadiumList();
+    void setStadiumList(const std::vector<Stadium>&);
+    StadiumList(const StadiumList&);
+    virtual ~StadiumList();
+
+signals:
+
+public slots:
+private:
+    std::vector<Stadium> m_stadiumList;
 };
 
 #endif // STADIUMLIST_HPP
