c252150acd2a7e5077ae20eb809fc8e35a1d1f6a
Christopher Rzechula <christopherrzechula@gmail.com>
2019-04-18 17:24:31 -0700

    Start some functionality (doesn't compile)

--- src/views/adminview.cpp
+++ src/views/adminview.cpp
@@ -1,13 +1,107 @@
 #include "adminview.hpp"
 #include "ui_adminview.h"
+#include "src/datastore/database.hpp"
 
 AdminView::AdminView(QWidget* parent)
     : View(parent), m_ui(new Ui::AdminView)
 {
     m_ui->setupUi(this);
+
+    /* Initialize souvenir lists */
+    m_availableSouvenirList = new SouvenirList(m_ui->widget_souvAvailableList);
+    m_hiddenSouvenirList = new SouvenirList(m_ui->widget_souvHiddenList);
+    m_hiddenSouvenirList->allowHidden(true);
 }
 
+/**
+ * Deletes all the widgets in the view.
+ */
 AdminView::~AdminView()
 {
     delete m_ui;
+    delete m_availableSouvenirList;
+    delete m_hiddenSouvenirList;
+}
+
+/**
+ * Resets the stacked widget page and UI by calling @a resetUi().
+ */
+void AdminView::resetView()
+{
+    //Change to stadium list page
+    m_ui->stackedWidget->setCurrentIndex(0);
+
+    m_currentStadiumId = -1;
+
+    resetUi();
+}
+
+/**
+ * Resets the UI to their original state.
+ */
+void AdminView::resetUi()
+{
+    loadSouvenirLists(m_currentStadiumId);
+}
+
+/**
+ * @brief Clear and load souvenir lists
+ *
+ * A stadium object is extracted from the database using @a stadiumId.
+ * The souvenirs of the extracted stadium is used to fill the
+ * available and hidden souvenir lists. As a precaution, the lists
+ * are cleared to remove any leftover items in the lists.
+ *
+ * If the passed ID results in an invalid stadium, this function does nothing.
+ *
+ * @param stadiumId Stadium ID with souvenirs to use in the lists
+ */
+void AdminView::loadSouvenirLists(int stadiumId)
+{
+    /* Extract stadium from database and check if valid */
+    const Stadium& stadium = Database::findStadiumById(stadiumId);
+    if(stadium.getId() == -1)
+        return;
+
+    /* Clear current lists */
+    m_availableSouvenirList->clear();
+    m_hiddenSouvenirList->clear();
+
+    /* Get the stadium and add each souvenir into the lists */
+    for(const Souvenir& souvenir : stadium.getSouvenirs())
+    {
+        if(souvenir.hidden)
+            m_hiddenSouvenirList->addItem(stadium.getId(), souvenir);
+        else
+            m_availableSouvenirList->addItem(stadium.getId(), souvenir);
+    }
+}
+
+/**
+ * @brief Fill stadium edit fields
+ *
+ *
+ *
+ * @param stadiumId Stadium ID to fill the edit fields with
+ */
+void AdminView::fillStadiumEditFields(int stadiumId)
+{
+    /* Extract stadium from database and check if valid */
+    const Stadium& stadium = Database::findStadiumById(stadiumId);
+//    const Team& team = Database::findTeamById(stadiumId.getTeamId());
+    if(stadium.getId() == -1)
+        return;
+
+    m_ui->lineEdit_stadName->setText(QString::fromStdString(stadium.getName()));
+    m_ui->lineEdit_stadLocation->setText(QString::fromStdString(stadium.getLocation()));
+    m_ui->lineEdit_stadTeamName->setText("wuhoh");
+    m_ui->comboBox_stadLeague->
+}
+
+/**
+ * Goes back to stadium list page by calling @a resetView().
+ */
+void AdminView::on_pushButton_souvGoBack_clicked()
+{
+    resetView();
 }
--- src/views/adminview.hpp
+++ src/views/adminview.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "view.hpp"
+#include "src/widgets/souvenirlist.hpp"
 
 namespace Ui
 {
@@ -11,12 +12,31 @@ class AdminView : public View
     Q_OBJECT
 
 public:
+    /* Constructors */
     explicit AdminView(QWidget* parent);
+
+    /* Destructor */
     ~AdminView() override;
 
-    void resetView() override {}
-    void resetUi() override {}
+    /* Resets */
+    void resetView() override;
+    void resetUi() override;
+
+private slots:
+    void loadSouvenirLists(int stadiumId);
+
+    /* Stadium subview controls */
+    void fillStadiumEditFields(int stadiumId);
+
+    /* Souvenir subview controls */
+    void fillSouvenirEditFields(int souvenirId);
+    void on_pushButton_souvGoBack_clicked();
 
 private:
     Ui::AdminView* m_ui;
+    int m_currentStadiumId = -1;
+
+    /* Widgets */
+    SouvenirList* m_availableSouvenirList;
+    SouvenirList* m_hiddenSouvenirList;
 };
