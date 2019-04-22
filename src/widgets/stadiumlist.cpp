23cb533adbc40ff6b9be7ff77cb92cdcea7f5415
AJVer <ajverburg@hotmail.com>
2019-04-21 23:37:13 -0700

    Got rid of invalid check

--- src/widgets/stadiumlist.cpp
+++ src/widgets/stadiumlist.cpp
@@ -69,26 +69,24 @@ void StadiumList::populateWidget(const std::vector<std::pair<Team,Stadium>>& sta
     QStringList headers = { "Team Name", "League", "Stadium Name", "Location", "Date Opened",
     "Seating Capacity", "Typology", "Roof Type", "Playing Surface", "Distance to Center Field"};
     m_listDisplay->setHeaderLabels(headers);
+    m_listDisplay->setSortingEnabled(false);
     for(std::pair<Team,Stadium> stadiumAndTeam : stadiumsAndTeams)
     {
-        if(stadiumAndTeam.first.getId() != -1 && stadiumAndTeam.second.getId() != -1)
-        {
-            //Adds all fields from stadium and team to the tree widget by converting them Qstrings
-            QStringList tmpList;
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.first.getName()));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.first.LEAGUE_STRING[stadiumAndTeam.first.league]));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.getName()));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.getLocation()));
-            tmpList.push_back(QString::fromStdString(std::to_string(stadiumAndTeam.second.getYearOpened())));
-            tmpList.push_back(QString::fromStdString(commaSeparate(std::to_string(stadiumAndTeam.second.getSeatCap()))));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.TYPOLOGY_STRING[stadiumAndTeam.second.typology]));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.ROOF_STRING[stadiumAndTeam.second.roof]));
-            tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.SURFACE_STRING[stadiumAndTeam.second.surface]));
-            tmpList.push_back(QString::fromStdString(commaSeparate(std::to_string(stadiumAndTeam.second.getCenterFieldDist()))));
+        //Adds all fields from stadium and team to the tree widget by converting them Qstrings
+        QStringList tmpList;
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.first.getName()));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.first.LEAGUE_STRING[stadiumAndTeam.first.league]));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.getName()));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.getLocation()));
+        tmpList.push_back(QString::fromStdString(std::to_string(stadiumAndTeam.second.getYearOpened())));
+        tmpList.push_back(QString::fromStdString(commaSeparate(std::to_string(stadiumAndTeam.second.getSeatCap()))));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.TYPOLOGY_STRING[stadiumAndTeam.second.typology]));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.ROOF_STRING[stadiumAndTeam.second.roof]));
+        tmpList.push_back(QString::fromStdString(stadiumAndTeam.second.SURFACE_STRING[stadiumAndTeam.second.surface]));
+        tmpList.push_back(QString::fromStdString(commaSeparate(std::to_string(stadiumAndTeam.second.getCenterFieldDist()))));
 
-            //Constructs a new stadium list item and adds it to the tree widget
-            new StadiumListItem(m_listDisplay,stadiumAndTeam.second,tmpList);
-        }
+        //Constructs a new stadium list item and adds it to the tree widget
+         new StadiumListItem(m_listDisplay,stadiumAndTeam.second,tmpList);
     }
     //Does not allow users to edit the cells directly
     m_listDisplay->setSortingEnabled(true);
